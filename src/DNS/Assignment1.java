72f8cd67bb231f530fa378f8a95d7645d29cd424
yuukinokai <sophie_deng97@hotmail.com>
2020-02-12 15:20:10 -0500

    add ipaddress  in byte

--- src/DNS/Assignment1.java
+++ src/DNS/Assignment1.java
@@ -6,6 +6,7 @@ public class Assignment1 {
 	static private int port = 53;
 	static private QueryType queryType = QueryType.A;
 	static private String name;
+	static private byte[] address = new byte[4];
 	static private String serverAddress; //IPv4 address of the DNS server
 	
 	
@@ -38,6 +39,18 @@ public class Assignment1 {
         		}
         		else if(arg.charAt(0) == '@') {
         			serverAddress = arg.substring(1);
+        			String[] serverSplit = serverAddress.split("\\.");
+        			if(serverSplit.length != 4) {
+        				throw new Exception("Wrong IP Address formatn (a.b.c.d).");
+        			}
+        			for(int j = 0; i < 4; i++) {
+        				int ip = Integer.parseInt(serverSplit[i]);
+                        if (ip < 0 || ip > 255) {
+                            throw new NumberFormatException("IP Address numbers must be between 0 and 255.");
+                        }
+                        address[i] = (byte) ip;
+        			}
+        			
         		}
         		else if(i == argsLength - 1){
         			name = arg;
